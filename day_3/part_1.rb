97d18d0beedf79b0ec312bb5af9258ab90da9f4c
Agis Anastasopoulos <a@xz0.org>
2021-12-12 18:34:26 +0200

    Day 3, part1 refactor

--- day_3/part_1.rb
+++ day_3/part_1.rb
@@ -2,7 +2,7 @@ scores = Hash.new(0)
 lines = ARGF.to_a.map(&:chomp)
 
 lines.each do |l|
-  l.each_char.each_with_index { |c, i| scores[i] += c.to_i }
+  l.chars.each_with_index { |c, i| scores[i] += c.to_i }
 end
 
 gamma = scores.map { |s| s[1] > lines.count/2 ? "1" : "0" }.join
